fc888b2ac1de02d60c955b52fc77d91521572b38
Rafael Schmitt <rafael.claycon@gmail.com>
2022-05-05 01:55:10 -0300

    Add MainView

---
+++ MedoDelirioBrasilia/MedoDelirioBrasilia/MainView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct MainView: View {
+
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        MainView()
+    }
+
+}
---
+++ MedoDelirioBrasilia/MedoDelirioBrasilia/MedoDelirioBrasiliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedoDelirioBrasiliaApp.swift
+//  MedoDelirioBrasilia
+//
+//  Created by Rafael Schmitt on 05/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MedoDelirioBrasiliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
