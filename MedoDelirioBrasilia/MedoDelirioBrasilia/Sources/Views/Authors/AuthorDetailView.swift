868d9c2e4ad7c825ae5995e6e705b5b0fa17f60e
Rafael Schmitt <rafael.claycon@gmail.com>
2022-05-28 13:50:05 -0300

    Add sounds to author detail view, add search to author list

--- MedoDelirioBrasilia/MedoDelirioBrasilia/Sources/Views/Authors/AuthorDetailView.swift
+++ MedoDelirioBrasilia/MedoDelirioBrasilia/Sources/Views/Authors/AuthorDetailView.swift
@@ -2,11 +2,44 @@ import SwiftUI
 
 struct AuthorDetailView: View {
 
+    @StateObject private var viewModel = AuthorDetailViewViewModel()
     @State var author: Author
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     
     var body: some View {
         VStack {
-            Text(author.name)
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 14) {
+                    ForEach(viewModel.sounds) { sound in
+                        SoundCell(soundId: sound.id, title: sound.title, author: sound.authorName ?? "", favorites: $viewModel.favoritesKeeper)
+                            .onTapGesture {
+                                viewModel.playSound(fromPath: sound.filename)
+                            }
+                            .onLongPressGesture {
+                                viewModel.soundForConfirmationDialog = sound
+                                viewModel.showConfirmationDialog = true
+                            }
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 7)
+                
+                Text("\(viewModel.sounds.count) sons")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .padding(.top, 10)
+                    .padding(.bottom, 18)
+            }
+        }
+        .navigationTitle(author.name)
+        .onAppear {
+            viewModel.reloadList(withSounds: soundData.filter({ $0.authorId == author.id }),
+                                 andFavorites: try? database.getAllFavorites(),
+                                 allowSensitiveContent: UserSettings.getShowOffensiveSounds())
         }
     }
 
