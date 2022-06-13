e10d060e13bbb3c45024e49aaec4dcd056ccc4b2
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-12 22:41:09 -0300

    Improve Settings screen

--- MedoDelirioBrasilia/MedoDelirioBrasilia/Sources/Views/Settings/SettingsView.swift
+++ MedoDelirioBrasilia/MedoDelirioBrasilia/Sources/Views/Settings/SettingsView.swift
@@ -70,7 +70,7 @@ struct SettingsView: View {
                                 showPixKeyCopiedAlert = true
                             }) {
                                 Text(pixKey)
-                                    .font(.footnote)
+                                    .font(.subheadline)
                                     .bold()
                             }
                             .tint(.blue)
@@ -81,9 +81,14 @@ struct SettingsView: View {
                             .alert(isPresented: $showPixKeyCopiedAlert) {
                                 Alert(title: Text("Chave copiada com sucesso!"), dismissButton: .default(Text("OK")))
                             }
+                            
+                            Text("**ÚLTIMA DOAÇÃO: SÉRGIO M. O.   R$ 20,00**")
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.center)
+                                .padding(.top, 15)
                         }
                         
-                        VStack(alignment: .center, spacing: 5) {
+                        /*VStack(alignment: .center, spacing: 5) {
                             Text("Gostou do que viu?")
                                 .multilineTextAlignment(.center)
                                 .padding(.horizontal)
@@ -111,10 +116,10 @@ struct SettingsView: View {
                             .buttonStyle(.bordered)
                             .buttonBorderShape(.capsule)
                             .padding(.top)
-                        }
+                        }*/
                         
                         VStack(alignment: .center, spacing: 5) {
-                            Text("Encontrou um problema ou gostaria de fazer uma sugestão?")
+                            Text("Encontrou um problema, gostaria de fazer uma sugestão ou pedir um som?")
                                 .multilineTextAlignment(.center)
                                 .padding(.horizontal)
                             
@@ -134,7 +139,7 @@ struct SettingsView: View {
                                 
                                 UIApplication.shared.open(mailToUrl)
                             }) {
-                                Text("Conte-nos por e-mail")
+                                Text("Fale com a gente por e-mail")
                             }
                             .tint(.pink)
                             .controlSize(.large)
