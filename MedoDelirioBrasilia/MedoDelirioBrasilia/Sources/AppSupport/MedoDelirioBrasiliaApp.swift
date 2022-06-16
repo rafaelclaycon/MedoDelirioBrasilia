c6dec82178f83c89136470fd6d5f19ce049ad45c
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-15 21:29:03 -0300

    Develop (#19)
    * Feat/add new sounds (#18)
    
    * Add 1 new sound
    
    * Add 9 new sounds, improve sound help screen
    
    * Fix Que merda hein sound author
    
    * Add 13 new sounds
    
    * Bump version

--- MedoDelirioBrasilia/MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
+++ MedoDelirioBrasilia/MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
@@ -4,7 +4,7 @@ var player: AudioPlayer?
 var database = LocalDatabase()
 let networkRabbit = NetworkRabbit(serverPath: "http://170.187.145.233:8080/api/")
 
-let soundsLastUpdateDate: String = "13/06/2022"
+let soundsLastUpdateDate: String = "15/06/2022"
 let songsLastUpdateDate: String = "23/05/2022"
 
 @main
