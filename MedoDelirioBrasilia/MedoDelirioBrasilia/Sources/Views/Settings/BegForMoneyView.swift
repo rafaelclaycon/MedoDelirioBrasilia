32d5503662d9e1645c813233396e03ce4525cb40
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-18 15:07:07 -0300

    Develop (#27)
    * Feat/add new sounds (#21)
    
    * Add author name to Olha só
    
    * Add 1 sound, fix Sound Not Found message
    
    * Add 6 sounds
    
    * Rebase develop (#25)
    
    * v1.3.4 Build 1 (#7)
    
    * Add 3 sounds by user request, add App Store review request infra
    
    * Finish App Store review prompt
    
    * Add server log models
    
    * Make first request to our server
    
    * Add post to server
    
    * Work on server stuff
    
    * More work on the server stuff
    
    * Fix tests
    
    * Remove user requested sounds to decouple them from Trends
    
    * Add user settings for each Trends sub-feature, add Trends Disabled View
    
    * Show Trends sections according to settings
    
    * Fix duplicate sound references
    
    * Fix duplicate sound references
    
    * Comment out Trends because it's not ready
    
    * Trying to fix sounds not playing
    
    * Readd missing sound
    
    * Send device model to server (#6)
    
    * Add send device model to server, fix sounds not playing
    
    * Add 5 new sounds
    
    * Develop (#9)
    
    * Add 3 sounds by user request, add App Store review request infra
    
    * Finish App Store review prompt
    
    * Add server log models
    
    * Make first request to our server
    
    * Add post to server
    
    * Work on server stuff
    
    * More work on the server stuff
    
    * Fix tests
    
    * Remove user requested sounds to decouple them from Trends
    
    * Add user settings for each Trends sub-feature, add Trends Disabled View
    
    * Show Trends sections according to settings
    
    * Fix duplicate sound references
    
    * Fix duplicate sound references
    
    * Comment out Trends because it's not ready
    
    * Trying to fix sounds not playing
    
    * Readd missing sound
    
    * Send device model to server (#6)
    
    * Add send device model to server, fix sounds not playing
    
    * Add 5 new sounds
    
    * Update README (#8)
    
    * v1.3.6 Build 3 (#13)
    
    * Feat/add new sounds (#12)
    
    * Add 3 new sounds
    
    * Add 8 new sounds
    
    * Disable auto correct on all search fields
    
    * Change tap on Created By behavior
    
    * Improve Settings screen
    
    * Fix sound last update date
    
    * Develop (#19)
    
    * Feat/add new sounds (#18)
    
    * Add 1 new sound
    
    * Add 9 new sounds, improve sound help screen
    
    * Fix Que merda hein sound author
    
    * Add 13 new sounds
    
    * Bump version
    
    * Feat/add new sounds (#21) (#22)
    
    * Add author name to Olha só
    
    * Add 1 sound, fix Sound Not Found message
    
    * Add 6 sounds
    
    * Fix/update readme (#24)
    
    * Feat/add new sounds (#21)
    
    * Add author name to Olha só
    
    * Add 1 sound, fix Sound Not Found message
    
    * Add 6 sounds
    
    * Update demo GIF
    
    * Redo asking for money (#26)

---
+++ MedoDelirioBrasilia/MedoDelirioBrasilia/Sources/Views/Settings/BegForMoneyView.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+
+struct BegForMoneyView: View {
+    
+    @State private var showPixKeyCopiedAlert: Bool = false
+    
+    let pixKey: String = "918bd609-04d1-4df6-8697-352b62462061"
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 18) {
+            
+            HStack(spacing: 15) {
+                Image("creator")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .frame(width: 80)
+                    //.shadow(radius: 3, y: 2)
+                
+                Text("Oi! Eu sou Rafael, o criador do app.")
+                    .multilineTextAlignment(.center)
+            }
+            
+            Text("Existem alguns custos para o app estar aqui. Especificamente a taxa de publicação (US$ 99/ano) e os custos mensais com servidor (cerca de R$ 30).")
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            
+            Text("Se você está curtindo o app e gostaria de ajudar, use a chave abaixo. Assim você garante novos sons, recursos e o meu muito obrigado!")
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            
+            Button(action: {
+                UIPasteboard.general.string = pixKey
+                showPixKeyCopiedAlert = true
+            }) {
+                Text(pixKey)
+                    .font(.subheadline)
+                    .bold()
+            }
+            .tint(.blue)
+            .controlSize(.large)
+            .buttonStyle(.bordered)
+            .buttonBorderShape(.roundedRectangle)
+            .alert(isPresented: $showPixKeyCopiedAlert) {
+                Alert(title: Text("Chave copiada com sucesso!"), dismissButton: .default(Text("OK")))
+            }
+            
+//            Text("**ÚLTIMA DOAÇÃO: SÉRGIO M. O.   R$ 20,00**")
+//                .foregroundColor(.gray)
+//                .multilineTextAlignment(.center)
+//                .padding(.top, 15)
+        }
+    }
+
+}
+
+struct BegForMoneyView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        BegForMoneyView()
+    }
+
+}
