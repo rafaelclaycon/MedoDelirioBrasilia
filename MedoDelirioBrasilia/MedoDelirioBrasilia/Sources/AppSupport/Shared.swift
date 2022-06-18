5f5a5376c8978df36aaf67a30abcb67d97a75f88
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-18 03:07:43 -0300

    Feat/add new sounds (#21)
    * Add author name to Olha só
    
    * Add 1 sound, fix Sound Not Found message
    
    * Add 6 sounds

--- MedoDelirioBrasilia/MedoDelirioBrasilia/Sources/AppSupport/Shared.swift
+++ MedoDelirioBrasilia/MedoDelirioBrasilia/Sources/AppSupport/Shared.swift
@@ -7,5 +7,8 @@ class Shared {
     static let viewSoundsByAuthorActivityTypeName = "com.rafaelschmitt.MedoDelirioBrasilia.ViewSoundsByAuthor"
     static let playAndShareSongsActivityTypeName = "com.rafaelschmitt.MedoDelirioBrasilia.PlayAndShareSongs"
     static let viewTrendsActivityTypeName = "com.rafaelschmitt.MedoDelirioBrasilia.ViewTrends"
+    
+    static let soundNotFoundAlertTitle = "Não Foi Possível Localizar Esse Som"
+    static let soundNotFoundAlertMessage = "Devido a um problema técnico, o som que você quer acessar não está disponível.\n\nPor favor, nos avise através do botão Fale com a Gente por E-mail na aba Ajustes."
 
 }
