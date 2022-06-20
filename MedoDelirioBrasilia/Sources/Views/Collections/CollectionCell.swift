b36f05da69ad39217ef99b5a9c6bbe9c39fad266
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-20 14:45:26 -0300

    Add Collection cell

---
+++ MedoDelirioBrasilia/Sources/Views/Collections/CollectionCell.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct CollectionCell: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+
+}
+
+struct CollectionCell_Previews: PreviewProvider {
+
+    static var previews: some View {
+        CollectionCell()
+    }
+
+}
