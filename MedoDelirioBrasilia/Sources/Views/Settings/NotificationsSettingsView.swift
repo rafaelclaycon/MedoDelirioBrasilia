28cfaaa1f98845d9585736b5ea778cd1220ed24d
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-21 19:32:45 -0300

    Organize Settings screen

---
+++ MedoDelirioBrasilia/Sources/Views/Settings/NotificationsSettingsView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct NotificationsSettingsView: View {
+
+    var body: some View {
+        Form {
+            Text("Hello, World!")
+        }
+    }
+
+}
+
+struct NotificationsSettingsView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        NotificationsSettingsView()
+    }
+
+}
