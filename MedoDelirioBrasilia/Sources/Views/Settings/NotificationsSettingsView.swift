e40cdfde7ad9e91f53047d58a7c85db6eea3aae0
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-21 22:03:28 -0300

    Add notification options

--- MedoDelirioBrasilia/Sources/Views/Settings/NotificationsSettingsView.swift
+++ MedoDelirioBrasilia/Sources/Views/Settings/NotificationsSettingsView.swift
@@ -2,10 +2,37 @@ import SwiftUI
 
 struct NotificationsSettingsView: View {
 
+    @State var newSounds = false
+    
     var body: some View {
         Form {
-            Text("Hello, World!")
+            Section {
+                Toggle("Novos sons", isOn: $newSounds)
+                    .onChange(of: newSounds) { newValue in
+                        //UserSettings.setEnableTrends(to: newValue)
+                    }
+                
+                Toggle("Novas músicas", isOn: $newSounds)
+                    .onChange(of: newSounds) { newValue in
+                        //UserSettings.setEnableTrends(to: newValue)
+                    }
+                
+                Toggle("Anúncios de novas funcionalidades", isOn: $newSounds)
+                    .onChange(of: newSounds) { newValue in
+                        //UserSettings.setEnableTrends(to: newValue)
+                    }
+                
+                Toggle("Brincadeiras", isOn: $newSounds)
+                    .onChange(of: newSounds) { newValue in
+                        //UserSettings.setEnableTrends(to: newValue)
+                    }
+            } header: {
+                Text("Escolha quais notificações deseja receber")
+            }
+            
         }
+        .navigationTitle("Notificações")
+        .navigationBarTitleDisplayMode(.inline)
     }
 
 }
