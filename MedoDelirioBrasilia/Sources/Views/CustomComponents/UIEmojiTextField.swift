25c560f0415752ba7d7ea1421dbf06223b8978fd
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-26 17:41:32 -0300

    Add EmojiTextField (still need to work out some kinks)

---
+++ MedoDelirioBrasilia/Sources/Views/CustomComponents/UIEmojiTextField.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class UIEmojiTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setEmoji() {
+        _ = self.textInputMode
+    }
+    
+    override var textInputContextIdentifier: String? {
+           return ""
+    }
+    
+    override var textInputMode: UITextInputMode? {
+        for mode in UITextInputMode.activeInputModes {
+            if mode.primaryLanguage == "emoji" {
+                self.keyboardType = .default
+                return mode
+            }
+        }
+        return nil
+    }
+
+}
