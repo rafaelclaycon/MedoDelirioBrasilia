dd39a96c8af49aeac6c4c866c48ad5237cdfb0b9
Rafael Schmitt <rafael.claycon@gmail.com>
2022-07-05 03:38:23 -0300

    Remove unneeded Bool flags from Settings

--- MedoDelirioBrasilia/Sources/Views/Settings/SettingsView.swift
+++ MedoDelirioBrasilia/Sources/Views/Settings/SettingsView.swift
@@ -4,14 +4,10 @@ struct SettingsView: View {
 
     @State private var showExplicitSounds: Bool = UserSettings.getShowOffensiveSounds()
     
-    @State private var showingTrendsSettingsScreen: Bool = false
-    
     @State private var showAskForMoneyView: Bool = false
     @State private var showPixKeyCopiedAlert: Bool = false
     
     @State private var showEmailAddressCopiedAlert: Bool = false
-    
-    @State private var showingDiagnosticsScreen: Bool = false
     
     let pixKey: String = "medodeliriosuporte@gmail.com"
     let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
@@ -34,7 +30,7 @@ struct SettingsView: View {
                         Text("Ajuda")
                     }
                     
-                    NavigationLink(destination: TrendsSettingsView(), isActive: $showingTrendsSettingsScreen) {
+                    NavigationLink(destination: TrendsSettingsView()) {
                         Text("Tendências")
                     }
                 }
@@ -104,7 +100,7 @@ struct SettingsView: View {
                 }
                 
                 Section("Diagnóstico") {
-                    NavigationLink(destination: DiagnosticsView(), isActive: $showingDiagnosticsScreen) {
+                    NavigationLink(destination: DiagnosticsView()) {
                         Text("Dados para nerds")
                     }
                 }
