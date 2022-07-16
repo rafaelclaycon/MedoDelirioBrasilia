199133907bd148a9552a054339990d215d02363c
Rafael Schmitt <rafael.claycon@gmail.com>
2022-07-16 16:15:33 -0300

    v2.6 (#80)
    * Improve Settings screen (#76)
    
    * New sounds and fixes (#77)
    
    * Fix 1 author name
    
    * Add glyphs to Sounds screen mode picker
    
    * Feature: Add iPad support (#78)
    
    * Add iPad support to project
    
    * Add sidebar view
    
    * Make views expand to the whole screen
    
    * Add more columns on grids for iPad
    
    * Add Mac Catalyst support
    
    * Add glyphs to sound list sort options
    
    * Update version number

--- MedoDelirioBrasilia/Sources/Views/Settings/SettingsView.swift
+++ MedoDelirioBrasilia/Sources/Views/Settings/SettingsView.swift
@@ -12,90 +12,88 @@ struct SettingsView: View {
     let pixKey: String = "medodeliriosuporte@gmail.com"
     
     var body: some View {
-        NavigationView {
-            Form {
+        Form {
+            Section {
+                Toggle("Exibir conteúdo sensível", isOn: $showExplicitSounds)
+                    .onChange(of: showExplicitSounds) { newValue in
+                        UserSettings.setShowOffensiveSounds(to: newValue)
+                    }
+            } footer: {
+                Text("Alguns conteúdos contam com muitos palavrões. Ao marcar essa opção, você concorda que tem mais de 18 anos e que deseja ver esse conteúdo.")
+            }
+            
+            Section {
+                NavigationLink(destination: HelpView()) {
+                    Text("Ajuda")
+                }
+                
+                NavigationLink(destination: TrendsSettingsView()) {
+                    Text("Tendências")
+                }
+            }
+            
+            Section("📬  Problemas, sugestões e pedidos") {
+                Button("Entrar em contato por e-mail") {
+                    showEmailClientConfirmationDialog = true
+                }
+            }
+            
+            Section {
+                PodcastAuthorsView()
+                    .padding(.vertical, 8)
+            }
+            
+            if showAskForMoneyView || CommandLine.arguments.contains("-UNDER_DEVELOPMENT") {
                 Section {
-                    Toggle("Exibir conteúdo sensível", isOn: $showExplicitSounds)
-                        .onChange(of: showExplicitSounds) { newValue in
-                            UserSettings.setShowOffensiveSounds(to: newValue)
-                        }
+                    BegForMoneyView()
+                        .padding(.vertical)
+                    
+                    Button("Copiar chave Pix") {
+                        UIPasteboard.general.string = pixKey
+                        showPixKeyCopiedAlert = true
+                    }
+                    .alert(isPresented: $showPixKeyCopiedAlert) {
+                        Alert(title: Text("Chave copiada com sucesso!"), dismissButton: .default(Text("OK")))
+                    }
+                } header: {
+                    Text("Ajude o app")
                 } footer: {
-                    Text("Alguns conteúdos contam com muitos palavrões. Ao marcar essa opção, você concorda que tem mais de 18 anos e que deseja ver esse conteúdo.")
-                }
-                
-                Section {
-                    NavigationLink(destination: HelpView()) {
-                        Text("Ajuda")
-                    }
-                    
-                    NavigationLink(destination: TrendsSettingsView()) {
-                        Text("Tendências")
-                    }
-                }
-                
-                Section("📬  Problemas, sugestões e pedidos") {
-                    Button("Entrar em contato por e-mail") {
-                        showEmailClientConfirmationDialog = true
-                    }
-                }
-                
-                Section {
-                    PodcastAuthorsView()
-                        .padding(.vertical, 8)
-                }
-                
-                if showAskForMoneyView || CommandLine.arguments.contains("-UNDER_DEVELOPMENT") {
-                    Section {
-                        BegForMoneyView()
-                            .padding(.vertical)
-                        
-                        Button("Copiar chave Pix") {
-                            UIPasteboard.general.string = pixKey
-                            showPixKeyCopiedAlert = true
-                        }
-                        .alert(isPresented: $showPixKeyCopiedAlert) {
-                            Alert(title: Text("Chave copiada com sucesso!"), dismissButton: .default(Text("OK")))
-                        }
-                    } header: {
-                        Text("Ajude o app")
-                    } footer: {
-                        Text("A chave é um endereço de e-mail, portanto, se o app do seu banco pedir o tipo de chave para fazer o Pix, selecione E-mail. Evite qualquer opção que mencione QR Code.")
-                    }
-                }
-                
-                Section("🧑‍💻  Contribua ou entenda como o app funciona") {
-                    Button(action: {
-                        let githubUrl = URL(string: "https://github.com/rafaelclaycon/MedoDelirioBrasilia")!
-                        UIApplication.shared.open(githubUrl)
-                    }) {
-                        Text("Ver código fonte no GitHub")
-                    }
-                }
-                
-                Section("Sobre") {
-                    Button("Criado por @claycon_") {
-                        guard let url = URL(string: "https://twitter.com/claycon_") else { return }
-                        UIApplication.shared.open(url)
-                    }
-                    
-                    Text("Versão \(Versioneer.appVersion) Build \(Versioneer.buildVersionNumber)")
-                }
-                
-                Section("Diagnóstico") {
-                    NavigationLink(destination: DiagnosticsView()) {
-                        Text("Dados para nerds")
-                    }
+                    Text("A chave é um endereço de e-mail, portanto, se o app do seu banco pedir o tipo de chave para fazer o Pix, selecione E-mail. Evite qualquer opção que mencione QR Code.")
                 }
             }
-            .navigationTitle("Ajustes")
-            .onAppear {
-                networkRabbit.displayAskForMoneyView { result, _ in
-                    showAskForMoneyView = result
+            
+            Section("🧑‍💻  Contribua ou entenda como o app funciona") {
+                Button(action: {
+                    let githubUrl = URL(string: "https://github.com/rafaelclaycon/MedoDelirioBrasilia")!
+                    UIApplication.shared.open(githubUrl)
+                }) {
+                    Text("Ver código fonte no GitHub")
                 }
             }
-            .confirmationDialog(Shared.pickAMailApp, isPresented: $showEmailClientConfirmationDialog, titleVisibility: .visible) {
-                Mailman.getMailClientOptions(subject: Shared.issueSuggestionEmailSubject, body: Shared.issueSuggestionEmailBody)
+            
+            Section("Sobre") {
+                Button("Criado por @claycon_") {
+                    guard let url = URL(string: "https://twitter.com/claycon_") else { return }
+                    UIApplication.shared.open(url)
+                }
+                
+                Text("Versão \(Versioneer.appVersion) Build \(Versioneer.buildVersionNumber)")
             }
+            
+            Section("Diagnóstico") {
+                NavigationLink(destination: DiagnosticsView()) {
+                    Text("Dados para nerds")
+                }
+            }
+        }
+        .navigationTitle("Ajustes")
+        .onAppear {
+            networkRabbit.displayAskForMoneyView { result, _ in
+                showAskForMoneyView = result
+            }
+        }
+        .confirmationDialog(Shared.pickAMailApp, isPresented: $showEmailClientConfirmationDialog, titleVisibility: .visible) {
+            Mailman.getMailClientOptions(subject: Shared.issueSuggestionEmailSubject, body: Shared.issueSuggestionEmailBody)
         }
     }
 
