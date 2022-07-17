24c84b9dd05fe2a3c94ead94362f2e4c96de0a58
Rafael Schmitt <rafael.claycon@gmail.com>
2022-07-17 18:46:14 -0300

    Add new share sheet to iPad

---
+++ MedoDelirioBrasilia/Sources/Views/Shared/ActivityViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+import SwiftUI
+
+struct ActivityViewController: UIViewControllerRepresentable {
+
+    var activityItems: [Any]
+    var completionWithItemsHandler: UIActivityViewController.CompletionWithItemsHandler?
+    var applicationActivities: [UIActivity]? = nil
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        controller.completionWithItemsHandler = completionWithItemsHandler
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ActivityViewController>) {}
+
+}
