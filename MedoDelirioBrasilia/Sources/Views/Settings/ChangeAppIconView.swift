011e807518aad8c5dbce6957f3f69623371eb6ca
Rafael Schmitt <rafael.claycon@gmail.com>
2022-08-21 15:17:46 -0300

    Add icon chooser screen

---
+++ MedoDelirioBrasilia/Sources/Views/Settings/ChangeAppIconView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ChangeAppIconView: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+
+}
+
+struct ChangeAppIconView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        ChangeAppIconView()
+    }
+
+}
