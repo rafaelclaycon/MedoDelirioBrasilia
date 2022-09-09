c78ce881ee0ef8244d0a8d9b4225061cf9467cf5
Rafael Schmitt <rafael.claycon@gmail.com>
2022-09-08 23:11:18 -0300

    Fix bundle identifier

--- MedoWidget/MedoWidget.swift
+++ MedoWidget/MedoWidget.swift
@@ -1,26 +1,20 @@
-//
-//  MedoWidget.swift
-//  MedoWidget
-//
-//  Created by Sergio Fernandes on 30/08/22.
-//
-
 import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
+
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date())
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date())
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
@@ -28,32 +22,35 @@ struct Provider: TimelineProvider {
             let entry = SimpleEntry(date: entryDate)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
+    
 }
 
 struct SimpleEntry: TimelineEntry {
+
     let date: Date
+    
 }
 
 struct MedoWidgetEntryView : View {
-    
+
     @Environment(\.widgetFamily) var widgetFamily
     var entry: Provider.Entry
-
+    
     var body: some View {
         switch widgetFamily {
-       // case .systemSmall:
+            // case .systemSmall:
             
-      //  case .systemMedium:
+            //  case .systemMedium:
             
-      //  case .systemLarge:
+            //  case .systemLarge:
             
-      //  case .systemExtraLarge:
+            //  case .systemExtraLarge:
             
-      //  case .accessoryCorner:
+            //  case .accessoryCorner:
             
         case .accessoryCircular:
             Image("figure.walk.departure")
@@ -69,48 +66,47 @@ struct MedoWidgetEntryView : View {
             .gaugeStyle(.accessoryCircular)
             
         case .accessoryRectangular:
-            
             VStack(alignment: .leading) {
                 Text("40 dias")
                     .bold()
                     .font(.system(size: 14))
-        
+                
                 Text("Primeiro Turno")
                     .textCase(.uppercase)
                     .font(.system(size: 12))
                     .fontWeight(.medium)
-                    
+                
                 
                 Text("131 dias")
                     .bold()
                     .font(.system(size: 14))
-        
+                
                 Text("Fim do Governo")
                     .textCase(.uppercase)
                     .font(.system(size: 12))
                     .fontWeight(.medium)
                 
-            }.frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
             
         case .accessoryInline:
-            
             HStack {
                 Image(systemName: "calendar")
                 Text("40 dias para o 1º turno")
-                
-                
             }
+            
         default:
             Text("Not implemented")
-            
         }
     }
+
 }
 
 @main
 struct MedoWidget: Widget {
-    let kind: String = "MedoWidget"
 
+    let kind: String = "MedoWidget"
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             MedoWidgetEntryView(entry: entry)
@@ -119,9 +115,11 @@ struct MedoWidget: Widget {
         .description("This is an example widget.")
         .supportedFamilies([.accessoryInline, .accessoryCircular, .accessoryRectangular])
     }
+    
 }
 
 struct MedoWidget_Previews: PreviewProvider {
+
     static var previews: some View {
         MedoWidgetEntryView(entry: SimpleEntry(date: Date()))
             .previewContext(WidgetPreviewContext(family: .accessoryInline))
@@ -135,4 +133,5 @@ struct MedoWidget_Previews: PreviewProvider {
             .previewContext(WidgetPreviewContext(family: .accessoryRectangular))
             .previewDisplayName("Rectangular")
     }
+
 }
