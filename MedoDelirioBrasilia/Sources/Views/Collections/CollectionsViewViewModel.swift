c9ec1aed8ad5cc8f3f9b272b67d7d6a9dcf5ea21
Rafael Schmitt <rafael.claycon@gmail.com>
2022-09-26 12:47:04 -0300

    Feature: Folders on the sidebar for iPad and Mac (#90)
    * Re-add sidebar modifications
    
    * Move Folder list outside Collections view
    
    * General improvements to Folder views
    
    * Add Folder list reload after creating
    
    * Load folders on the sidebar
    
    * Reload folders on the sidebar
    
    * Fix Folders are tags banner
    
    * Small fixes

--- MedoDelirioBrasilia/Sources/Views/Collections/CollectionsViewViewModel.swift
+++ MedoDelirioBrasilia/Sources/Views/Collections/CollectionsViewViewModel.swift
@@ -5,16 +5,11 @@ class CollectionsViewViewModel: ObservableObject {
 
     @Published var collections = [ContentCollection]()
     
-    @Published var folders = [UserFolder]()
-    @Published var hasFoldersToDisplay: Bool = false
-    
-    @Published var currentActivity: NSUserActivity? = nil
-    
     // Alerts
-    @Published var alertTitle: String = ""
-    @Published var alertMessage: String = ""
-    @Published var showAlert: Bool = false
-    @Published var folderIdForDeletion: String = ""
+//    @Published var alertTitle: String = ""
+//    @Published var alertMessage: String = ""
+//    @Published var showAlert: Bool = false
+//    @Published var folderIdForDeletion: String = ""
     
     func reloadCollectionList(withCollections outsideCollections: [ContentCollection]?) {
         guard let actualCollections = outsideCollections, actualCollections.count > 0 else {
@@ -23,26 +18,13 @@ class CollectionsViewViewModel: ObservableObject {
         self.collections = actualCollections
     }
     
-    func reloadFolderList(withFolders outsideFolders: [UserFolder]?) {
-        guard let actualFolders = outsideFolders, actualFolders.count > 0 else {
-            return hasFoldersToDisplay = false
-        }
-        self.folders = actualFolders
-        self.hasFoldersToDisplay = true
-    }
-    
-    func donateActivity() {
-        self.currentActivity = UserActivityWaiter.getDonatableActivity(withType: Shared.ActivityTypes.viewCollections, andTitle: "Ver e criar pastas de sons")
-        self.currentActivity?.becomeCurrent()
-    }
-    
     // MARK: - Alerts
     
-    func showFolderDeletionConfirmation(folderName: String, folderId: String) {
-        alertTitle = "Apagar a Pasta \"\(folderName)\"?"
-        alertMessage = "Os sons continuarão disponíveis no app, fora da pasta.\n\nEssa ação não pode ser desfeita."
-        folderIdForDeletion = folderId
-        showAlert = true
-    }
+//    func showFolderDeletionConfirmation(folderName: String, folderId: String) {
+//        alertTitle = "Apagar a Pasta \"\(folderName)\"?"
+//        alertMessage = "Os sons continuarão disponíveis no app, fora da pasta.\n\nEssa ação não pode ser desfeita."
+//        folderIdForDeletion = folderId
+//        showAlert = true
+//    }
 
 }
