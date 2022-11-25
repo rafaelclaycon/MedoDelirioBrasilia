2246e8d440a4d134226b2af72c1261c6cc8d5a82
Rafael Schmitt <rafael.claycon@gmail.com>
2022-11-25 16:22:04 -0300

    Add dummy call

--- MedoDelirioBrasilia/Sources/Views/Collections/CollectionList/CollectionListViewViewModel.swift
+++ MedoDelirioBrasilia/Sources/Views/Collections/CollectionList/CollectionListViewViewModel.swift
@@ -25,15 +25,18 @@ class CollectionListViewViewModel: ObservableObject {
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             guard let self = self else { return }
             
-            FolderResearchHelper.sendLogs { success in
-                DispatchQueue.main.async {
-                    if success {
-                        self.state = .displayingData
-                    } else {
-                        self.state = .loadingError
-                    }
-                }
-            }
+            self.collections = self.getLocalCollections()
+            self.state = .displayingData
+            
+//            FolderResearchHelper.sendLogs { success in
+//                DispatchQueue.main.async {
+//                    if success {
+//                        self.state = .displayingData
+//                    } else {
+//                        self.state = .loadingError
+//                    }
+//                }
+//            }
         }
     }
     
@@ -48,6 +51,16 @@ class CollectionListViewViewModel: ObservableObject {
             return
         }
         self.collections = actualCollections
+    }
+    
+    private func getLocalCollections() -> [ContentCollection] {
+        var array = [ContentCollection]()
+        array.append(ContentCollection(title: "LGBT", imageURL: "http://blog.saude.mg.gov.br/wp-content/uploads/2021/06/28-06-lgbt.jpg"))
+        array.append(ContentCollection(title: "Clássicos", imageURL: "https://www.avina.net/wp-content/uploads/2019/06/Confiamos-no-Brasil-e-nos-brasileiros-e-brasileiras.jpg"))
+        array.append(ContentCollection(title: "Sérios", imageURL: "https://images.trustinnews.pt/uploads/sites/5/2019/10/tres-tabus-que-o-homem-atual-ja-ultrapassou-2.jpeg"))
+        array.append(ContentCollection(title: "Invasão Foro", imageURL: "https://i.scdn.co/image/0a32a3b9a4f798833f1c10aac18197f7b119e758"))
+        array.append(ContentCollection(title: "Memes", imageURL: "https://i.ytimg.com/vi/r0jh29F6hSs/mqdefault.jpg"))
+        return array
     }
     
     // MARK: - Alerts
