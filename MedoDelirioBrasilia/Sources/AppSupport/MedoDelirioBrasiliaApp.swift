d27cef365c509f2f6b5668779eab86c659288015
Rafael Schmitt <rafael.claycon@gmail.com>
2022-12-08 13:29:54 -0300

    Bump version number for App Store release

--- MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
+++ MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
@@ -8,7 +8,7 @@ let networkRabbit = NetworkRabbit(serverPath: CommandLine.arguments.contains("-U
 let podium = Podium(database: database, networkRabbit: networkRabbit)
 
 let soundsLastUpdateDate: String = "08/12/2022"
-let songsLastUpdateDate: String = "07/11/2022"
+let songsLastUpdateDate: String = "08/12/2022"
 
 var moveDatabaseIssue: String = .empty
 
