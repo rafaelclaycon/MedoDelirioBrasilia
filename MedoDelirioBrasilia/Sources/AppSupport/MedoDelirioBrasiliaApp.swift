c8447f665861666a9648fded2b0501ddb3a3ad66
Rafael Schmitt <rafael.claycon@gmail.com>
2022-12-08 12:34:25 -0300

    Add 2 new sounds, add 1 new song

--- MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
+++ MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
@@ -7,7 +7,7 @@ var database = LocalDatabase()
 let networkRabbit = NetworkRabbit(serverPath: CommandLine.arguments.contains("-UNDER_DEVELOPMENT") ? "http://127.0.0.1:8080/api/" : "http://170.187.145.233:8080/api/")
 let podium = Podium(database: database, networkRabbit: networkRabbit)
 
-let soundsLastUpdateDate: String = "30/11/2022"
+let soundsLastUpdateDate: String = "08/12/2022"
 let songsLastUpdateDate: String = "07/11/2022"
 
 var moveDatabaseIssue: String = .empty
