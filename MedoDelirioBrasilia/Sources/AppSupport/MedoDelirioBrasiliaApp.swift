ccb074529af89484f815ae783bf171c3320ee958
Rafael Schmitt <rafael.claycon@gmail.com>
2023-02-11 14:28:31 -0300

    Add 1 new song

--- MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
+++ MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
@@ -8,7 +8,7 @@ let networkRabbit = NetworkRabbit(serverPath: CommandLine.arguments.contains("-U
 let podium = Podium(database: database, networkRabbit: networkRabbit)
 
 let soundsLastUpdateDate: String = "11/02/2023"
-let songsLastUpdateDate: String = "07/02/2023"
+let songsLastUpdateDate: String = "11/02/2023"
 
 var moveDatabaseIssue: String = .empty
 
