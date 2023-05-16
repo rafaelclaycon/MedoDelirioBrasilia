c62543bbd2fabd8653eeda5496263f7fade00d0b
Rafael Schmitt <rafael.claycon@gmail.com>
2023-05-16 17:44:35 -0300

    Replace split sound data in SoundCell for whole Sound

--- MedoDelirioBrasilia/Sources/Views/Folders/FolderDetail/FolderDetailView.swift
+++ MedoDelirioBrasilia/Sources/Views/Folders/FolderDetail/FolderDetailView.swift
@@ -63,7 +63,7 @@ struct FolderDetailView: View {
                                 
                                 LazyVGrid(columns: columns, spacing: UIDevice.current.userInterfaceIdiom == .phone ? 14 : 20) {
                                     ForEach(viewModel.sounds) { sound in
-                                        SoundCell(soundId: sound.id, title: sound.title, author: sound.authorName ?? "", duration: sound.duration, isNew: sound.isNew ?? false, favorites: .constant(Set<String>()), highlighted: .constant(Set<String>()), nowPlaying: $viewModel.nowPlayingKeeper, selectedItems: $viewModel.selectionKeeper, currentSoundsListMode: $viewModel.currentSoundsListMode.wrappedValue)
+                                        SoundCell(sound: sound, favorites: .constant(Set<String>()), highlighted: .constant(Set<String>()), nowPlaying: $viewModel.nowPlayingKeeper, selectedItems: $viewModel.selectionKeeper, currentSoundsListMode: $viewModel.currentSoundsListMode.wrappedValue)
                                             .contentShape(.contextMenuPreview, RoundedRectangle(cornerRadius: 20, style: .continuous))
                                             .padding(.horizontal, UIDevice.current.userInterfaceIdiom == .phone ? 0 : 5)
                                             .onTapGesture {
