6dd1622851c400671ddf6fd7d42ccfb01b26ce44
Rafael Schmitt <rafael.claycon@gmail.com>
2023-05-25 09:50:40 -0300

    Fix 1 author name

--- MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
+++ MedoDelirioBrasilia/Sources/AppSupport/MedoDelirioBrasiliaApp.swift
@@ -7,7 +7,7 @@ var database = LocalDatabase()
 let networkRabbit = NetworkRabbit(serverPath: CommandLine.arguments.contains("-UNDER_DEVELOPMENT") ? "http://127.0.0.1:8080/api/" : "http://medodelirioios.lat:8080/api/")
 let podium = Podium(database: database, networkRabbit: networkRabbit)
 
-let soundsLastUpdateDate: String = "17/05/2023"
+let soundsLastUpdateDate: String = "25/05/2023"
 let songsLastUpdateDate: String = "16/04/2023"
 
 var moveDatabaseIssue: String = .empty
