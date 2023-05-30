e7336d19fd4a5658725a2a16208bf408e44a2c52
Rafael Schmitt <rafael.claycon@gmail.com>
2023-05-30 17:04:29 -0300

    Working on Author CRUD

--- MedoDelirioBrasilia/Sources/Networking/Services/SyncService+Author.swift
+++ MedoDelirioBrasilia/Sources/Networking/Services/SyncService+Author.swift
@@ -10,18 +10,17 @@ import Foundation
 extension SyncService {
     
     func createAuthor(from updateEvent: UpdateEvent) async {
-//        let url = URL(string: networkRabbit.serverPath + "v3/sound/\(updateEvent.contentId)")!
-//        do {
-//            let sound: Sound = try await NetworkRabbit.get(from: url)
-//            try injectedDatabase.insert(sound: sound)
-//            
-//            let fileUrl = URL(string: "http://127.0.0.1:8080/sounds/\(updateEvent.contentId).mp3")!
-//            let downloadedFileUrl = try await NetworkRabbit.downloadFile(from: fileUrl, into: InternalFolderNames.downloadedSounds)
-//            print("File downloaded successfully at: \(downloadedFileUrl)")
-//        } catch {
-//            print(error)
-//            print(error.localizedDescription)
-//        }
+        let url = URL(string: networkRabbit.serverPath + "v3/sound/\(updateEvent.contentId)")!
+        do {
+            let sound: Sound = try await NetworkRabbit.get(from: url)
+            
+            try injectedDatabase.insert(sound: sound)
+            
+            try injectedDatabase.markAsSucceeded(updateEventId: updateEvent.id)
+        } catch {
+            print(error)
+            Logger.logSyncError(description: error.localizedDescription, updateEventId: updateEvent.id.uuidString)
+        }
     }
     
     func updateAuthorMetadata(with updateEvent: UpdateEvent) {
