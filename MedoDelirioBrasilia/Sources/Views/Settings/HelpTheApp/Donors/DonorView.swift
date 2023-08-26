8ea041cafea72b81ccff7f091e31f37fd6e873a8
Rafael Schmitt <rafael.claycon@gmail.com>
2023-08-26 16:31:14 -0300

    Add Recurring and Recurring+ tags for Donors

--- MedoDelirioBrasilia/Sources/Views/Settings/HelpTheApp/Donors/DonorView.swift
+++ MedoDelirioBrasilia/Sources/Views/Settings/HelpTheApp/Donors/DonorView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct DonorView: View {
-    
+
     @State var donor: Donor
     @Environment(\.colorScheme) var colorScheme
-    
+
     var body: some View {
         ZStack {
             Text(donor.name)
@@ -20,19 +20,38 @@ struct DonorView: View {
                 .padding(.horizontal, 12)
                 .padding(.vertical, 8)
                 .background(RoundedRectangle(cornerRadius: 9).fill(.gray).opacity(colorScheme == .dark ? 0.5 : 0.1))
-            
+
             if donor.hasDonatedBefore {
-                Text("⭐️  JÁ DOOU ANTES")
-                    .font(.caption)
-                    .foregroundColor(colorScheme == .dark ? .black : .white)
-                    .bold()
-                    .padding(.horizontal, 10)
-                    .padding(.vertical, 4)
-                    .background(RoundedRectangle(cornerRadius: 9).fill(.primary))
-                    .offset(y: 22)
+                SpecialDonorTag(text: "⭐️  JÁ DOOU ANTES", backgroundColor: .primary)
+            } else if donor.isRecurringDonorBelow30 ?? false {
+                SpecialDonorTag(text: "RECORRENTE", backgroundColor: .red)
+            } else if donor.isRecurringDonor30OrOver ?? false {
+                SpecialDonorTag(text: "✨RECORRENTE+✨", backgroundColor: .red)
             }
         }
         .fixedSize()
+    }
+}
+
+extension DonorView {
+
+    struct SpecialDonorTag: View {
+
+        let text: String
+        let backgroundColor: Color
+
+        @Environment(\.colorScheme) var colorScheme
+
+        var body: some View {
+            Text(text)
+                .font(.caption)
+                .foregroundColor(colorScheme == .dark ? .black : .white)
+                .bold()
+                .padding(.horizontal, 10)
+                .padding(.vertical, 4)
+                .background(RoundedRectangle(cornerRadius: 9).fill(backgroundColor))
+                .offset(y: 22)
+        }
     }
 }
 
@@ -41,7 +60,7 @@ struct DonorView_Previews: PreviewProvider {
     static var previews: some View {
         VStack(spacing: 50) {
             DonorView(donor: Donor(name: "Bruno P. G. P."))
-            DonorView(donor: Donor(name: "Clarissa P. S.", isRecurringDonor: true))
+            DonorView(donor: Donor(name: "Clarissa P. S.", hasDonatedBefore: true))
             DonorView(donor: Donor(name: "Douglas B."))
             DonorView(donor: Donor(name: "Pedro Henrique B. P."))
         }
