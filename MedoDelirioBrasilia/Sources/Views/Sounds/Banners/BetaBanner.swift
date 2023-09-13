38526fe6716ea9577a9cd4165d43faae07028e18
Rafael Schmitt <rafael.claycon@gmail.com>
2023-09-13 01:41:32 -0300

    Fix issues from merge

--- MedoDelirioBrasilia/Sources/Views/Sounds/Banners/BetaBanner.swift
+++ MedoDelirioBrasilia/Sources/Views/Sounds/Banners/BetaBanner.swift
@@ -32,7 +32,7 @@ struct BetaBanner: View {
                     .font(.callout)
 
                 Button {
-                    OpenUtility.open(link: testFlightLink)
+                    //OpenUtility.open(link: testFlightLink)
                 } label: {
                     Text("Baixar a versão Beta")
                 }
