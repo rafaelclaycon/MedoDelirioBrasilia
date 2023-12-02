af4a1ffcae1f731b0dced0bdc67788d898c43aa5
Rafael Schmitt <rafael.claycon@gmail.com>
2023-12-02 01:52:14 -0300

    Move SongCell background to background modifier

--- MedoDelirioBrasilia/Sources/Views/Songs/SongCell.swift
+++ MedoDelirioBrasilia/Sources/Views/Songs/SongCell.swift
@@ -44,89 +44,84 @@ struct SongCell: View {
     let gradient = LinearGradient(gradient: Gradient(colors: [.green, .green, .green, .brightYellow]), startPoint: .topLeading, endPoint: .bottomTrailing)
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20, style: .continuous)
-                .fill(gradient)
-                .frame(height: cellHeight)
-                .opacity(isPlaying ? 0.7 : 1.0)
-            
-            VStack(alignment: .leading, spacing: 8) {
-                HStack(spacing: 10) {
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(song.title)
-                            .foregroundColor(.black)
-                            .bold()
+        VStack(alignment: .leading, spacing: 8) {
+            HStack(spacing: 10) {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(song.title)
+                        .foregroundColor(.black)
+                        .bold()
+                        .multilineTextAlignment(.leading)
+                        .lineLimit(2)
+
+                    HStack(spacing: 10) {
+                        Text("\(song.genreName ?? "") · \(durationForDisplay)")
+                            .foregroundColor(.white)
+                            .font(.callout)
                             .multilineTextAlignment(.leading)
-                            .lineLimit(2)
-                        
-                        HStack(spacing: 10) {
-                            Text("\(song.genreName ?? "") · \(durationForDisplay)")
-                                .foregroundColor(.white)
-                                .font(.callout)
-                                .multilineTextAlignment(.leading)
-                                .onReceive(timer) { time in
-                                    guard isPlaying else { return }
-                                    if timeRemaining > 0 {
-                                        timeRemaining -= 1
-                                    }
+                            .onReceive(timer) { time in
+                                guard isPlaying else { return }
+                                if timeRemaining > 0 {
+                                    timeRemaining -= 1
                                 }
-                            
-                            if isNew {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                        .fill(.yellow)
-                                        .frame(width: 50, height: 20)
-                                    
-                                    Text("NOVA")
-                                        .foregroundColor(.black)
-                                        .font(.footnote)
-                                        .bold()
-                                        .opacity(0.7)
-                                }
+                            }
+
+                        if isNew {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                                    .fill(.yellow)
+                                    .frame(width: 50, height: 20)
+
+                                Text("NOVA")
+                                    .foregroundColor(.black)
+                                    .font(.footnote)
+                                    .bold()
+                                    .opacity(0.7)
                             }
                         }
                     }
-                    
-                    Spacer()
-                    
-                    if isPlaying {
-                        Image(systemName: "stop.circle")
-                            .font(.largeTitle)
-                            .foregroundColor(.black)
-                            .padding(.trailing)
-                    }
                 }
-            }
-            .padding(.leading, 20)
-            .onAppear {
-                if !isPlaying {
-                    timeRemaining = song.duration
-                }
-            }
-            .onChange(of: isPlaying) { isPlaying in
-                if !isPlaying {
-                    timeRemaining = song.duration
+
+                Spacer()
+
+                if isPlaying {
+                    Image(systemName: "stop.circle")
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                        .padding(.trailing)
                 }
             }
         }
+        .frame(height: cellHeight)
+        .padding(.leading, 20)
+        .onAppear {
+            if !isPlaying {
+                timeRemaining = song.duration
+            }
+        }
+        .onChange(of: isPlaying) { isPlaying in
+            if !isPlaying {
+                timeRemaining = song.duration
+            }
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(gradient)
+                .opacity(isPlaying ? 0.7 : 1.0)
+        }
     }
-
 }
 
-struct SongCell_Previews: PreviewProvider {
-
-    static var previews: some View {
-        SongCell(
-            song: Song(
-                id: "ABC",
-                title: "Funk do Morto",
-                genreId: "82BFAA10-C01A-4FE0-8366-1B1690D00A40",
-                genreName: "Funk",
-                duration: 60
-            ),
-            nowPlaying: .constant(Set<String>())
-        )
-        .padding(.horizontal)
-        .previewLayout(.fixed(width: 414, height: 100))
-    }
+#Preview {
+    SongCell(
+        song: Song(
+            id: "ABC",
+            title: "Funk do Morto",
+            genreId: "82BFAA10-C01A-4FE0-8366-1B1690D00A40",
+            genreName: "Funk",
+            duration: 60
+        ),
+        nowPlaying: .constant(Set<String>())
+    )
+    .padding(.horizontal)
+    .previewLayout(.fixed(width: 414, height: 100))
 }
