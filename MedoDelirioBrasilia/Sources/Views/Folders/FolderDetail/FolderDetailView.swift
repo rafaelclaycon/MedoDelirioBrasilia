a497cc0714119c97df04527419772941bcf91707
Rafael Schmitt <rafael.claycon@gmail.com>
2024-04-30 22:18:22 -0300

    Fix toast too high when no floating selector

--- MedoDelirioBrasilia/Sources/Views/Folders/FolderDetail/FolderDetailView.swift
+++ MedoDelirioBrasilia/Sources/Views/Folders/FolderDetail/FolderDetailView.swift
@@ -49,7 +49,7 @@ struct FolderDetailView: View {
                     menuOptions: [.sharingOptions(), .playFromThisSound(), .removeFromFolder()],
                     currentSoundsListMode: .constant(.regular)
                 ),
-                stopShowingFloatingSelector: .constant(false),
+                stopShowingFloatingSelector: .constant(nil),
                 emptyStateView: AnyView(
                     EmptyFolderView()
                         .padding(.horizontal, 30)
