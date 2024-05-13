4cf5678809c45598470ae37e3a611a16fa55bf6b
Rafael Schmitt <rafael.claycon@gmail.com>
2024-05-13 09:57:00 -0300

    Add folder exporting tools

--- MedoDelirioBrasilia/Sources/Views/Folders/FolderList/AllFoldersiPadView.swift
+++ MedoDelirioBrasilia/Sources/Views/Folders/FolderList/AllFoldersiPadView.swift
@@ -15,10 +15,24 @@ struct AllFoldersiPadView: View {
     @State var deleteFolderAide = DeleteFolderViewAide()
     @State var folderIdForEditing: String = .empty
     @StateObject var deleteFolderAideiPhone = DeleteFolderViewAideiPhone() // Not used, here just so FolderList does not crash on iPad
-    
+
+    // iPad Reactions Stuff
+    @State private var exportDataString: String = ""
+    @State private var isDataReadyToShow: Bool = false
+
+    // MARK: - View Body
+
     var body: some View {
         ScrollView {
             VStack(alignment: .center) {
+                if isDataReadyToShow {
+                    TextEditor(text: $exportDataString)
+                        .frame(minHeight: 300)  // Set a minimum height for the TextEditor
+                        .padding()
+                        .border(Color.gray, width: 1)  // Optionally add a border
+                        //.disabled(true)  // Make it non-editable
+                }
+
                 FolderList(
                     updateFolderList: $updateFolderList,
                     folderIdForEditing: $folderIdForEditing
@@ -36,7 +50,7 @@ struct AllFoldersiPadView: View {
                 HStack(spacing: 20) {
                     if CommandLine.arguments.contains("-SHOW_EXPORT_FOLDERS_OPTION") {
                         Button("Exportar Pastas") {
-                            print("")
+                            exportFolders()
                         }
                     }
 
@@ -68,6 +82,51 @@ struct AllFoldersiPadView: View {
         }
     }
 
+    // MARK: - Functions
+
+    private func exportFolders() {
+        do {
+            var rawFolders = try LocalDatabase.shared.getAllUserFolders()
+            var folders = rawFolders.map { UserFolderDTO(userFolder: $0) }
+
+            for i in folders.indices {
+                let folderContents = try LocalDatabase.shared.getAllContentsInsideUserFolder(withId: folders[i].id)
+                let contentIds = folderContents.map { $0.contentId }
+                let sounds = try LocalDatabase.shared.sounds(withIds: contentIds)
+                folders[i].sounds = sounds.map { $0.id }
+            }
+
+            if let jsonData = try? JSONEncoder().encode(folders), let jsonString = String(data: jsonData, encoding: .utf8) {
+                DispatchQueue.main.async {
+                    self.exportDataString = jsonString
+                    UIPasteboard.general.string = jsonString
+                    self.isDataReadyToShow = true
+                }
+            }
+        } catch {
+            print("Erro ao tentar exportar as pastas para arquivo: \(error.localizedDescription)")
+        }
+    }
+
+    private func verifyFile(at url: URL) -> Bool {
+        let fileManager = FileManager.default
+        if fileManager.fileExists(atPath: url.path) {
+            do {
+                let attributes = try fileManager.attributesOfItem(atPath: url.path)
+                if let fileSize = attributes[.size] as? NSNumber, fileSize.intValue > 0 {
+                    return true
+                }
+            } catch {
+                print("Error reading file attributes: \(error.localizedDescription)")
+            }
+        }
+        return false
+    }
+
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
 #Preview {
