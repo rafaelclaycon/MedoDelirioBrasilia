fbdfb84a384ab054b9475a26de87c5617d74989a
Rafael Schmitt <rafael.claycon@gmail.com>
2024-05-24 23:07:48 -0300

    Add Equatable conformance for LoadingState

--- MedoDelirioBrasilia/Sources/Models/SoundListDecoupling/LoadingState.swift
+++ MedoDelirioBrasilia/Sources/Models/SoundListDecoupling/LoadingState.swift
@@ -7,8 +7,22 @@
 
 import Foundation
 
-enum LoadingState<T> {
+enum LoadingState<T: Equatable>: Equatable {
+
     case loading
     case loaded([T])
     case error(String)
+
+    static func == (lhs: LoadingState<T>, rhs: LoadingState<T>) -> Bool {
+        switch (lhs, rhs) {
+        case (.loading, .loading):
+            return true
+        case (.loaded(let lhsItems), .loaded(let rhsItems)):
+            return lhsItems == rhsItems
+        case (.error(let lhsError), .error(let rhsError)):
+            return lhsError == rhsError
+        default:
+            return false
+        }
+    }
 }
