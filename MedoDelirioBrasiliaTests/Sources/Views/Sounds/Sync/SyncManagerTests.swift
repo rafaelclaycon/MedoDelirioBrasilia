b1d97aa917987525642b5619ff018bf8fe010a02
Rafael Schmitt <rafael.claycon@gmail.com>
2024-10-21 02:56:42 -0300

    Fix sync tests

--- MedoDelirioBrasiliaTests/Sources/Views/Sounds/Sync/SyncManagerTests.swift
+++ MedoDelirioBrasiliaTests/Sources/Views/Sounds/Sync/SyncManagerTests.swift
@@ -16,6 +16,14 @@ final class SyncManagerTests: XCTestCase {
     private var localDatabase: LocalDatabaseStub!
     private var logger: LoggerStub!
 
+    private let mockUpdates: [UpdateEvent] = [
+        .init(id: UUID(uuidString: "6AE46488-4872-4C71-84D7-E38A69F123DD")!, contentId: "1", mediaType: .sound, eventType: .created),
+        .init(id: UUID(uuidString: "EB73727F-E228-4025-9109-886A9C33CAC5")!, contentId: "1", mediaType: .sound, eventType: .created),
+        .init(id: UUID(uuidString: "DC15BD2A-88BE-4C4D-9AE5-790856FAC919")!, contentId: "1", mediaType: .sound, eventType: .created),
+        .init(id: UUID(uuidString: "22356F60-4F21-4847-821A-B4D228E3B161")!, contentId: "1", mediaType: .sound, eventType: .created),
+        .init(id: UUID(uuidString: "58604F80-8840-4858-87DD-65EF4D016C37")!, contentId: "1", mediaType: .sound, eventType: .created)
+    ]
+
     override func setUpWithError() throws {
         try super.setUpWithError()
         syncService = SyncServiceStub()
@@ -29,22 +37,50 @@ final class SyncManagerTests: XCTestCase {
         logger = nil
         try super.tearDownWithError()
     }
-
-
 }
 
 // MARK: - User Flows
 
 extension SyncManagerTests {
 
+    func testSync_whenNoInternetConnection_shouldReturnSyncError() async throws {
+        syncService.errorToThrowOnUpdate = .errorFetchingUpdateEvents("")
+
+        sut = SyncManager(service: syncService, database: localDatabase, logger: logger)
+
+        await sut.sync()
+
+        XCTAssertEqual(localDatabase.numberOfTimesInsertUpdateEventWasCalled, 0)
+        XCTAssertEqual(logger.errorHistory.count, 1)
+        XCTAssertEqual(logger.successHistory.count, 0)
+    }
+
+    func testSync_whenNoChanges_shouldReturnNothingToUpdate() async throws {
+        syncService.updates = []
+
+        sut = SyncManager(service: syncService, database: localDatabase, logger: logger)
+
+        await sut.sync()
+
+        XCTAssertEqual(localDatabase.numberOfTimesInsertUpdateEventWasCalled, 0)
+        XCTAssertEqual(logger.errorHistory.count, 0)
+        XCTAssertEqual(logger.successHistory.count, 1)
+    }
+
+    func testSync_whenSomeUpdates_shouldSuccessfullyExecuteExactNumberOfUpdates() async throws {
+        syncService.updates = mockUpdates
+
+        sut = SyncManager(service: syncService, database: localDatabase, logger: logger)
+
+        await sut.sync()
+
+        XCTAssertEqual(localDatabase.numberOfTimesInsertUpdateEventWasCalled, 5)
+        XCTAssertEqual(logger.errorHistory.count, 0)
+        XCTAssertEqual(logger.successHistory.count, 0) // Why is this zero?
+    }
+
     func testSync_whenAlreadyHasSomeSuccessfulUpdatesAndLastUpdateDateIsAllForSomeReason_shouldSaveOnlyNewUpdates() async throws {
-        syncService.updates = [
-            .init(id: UUID(uuidString: "6AE46488-4872-4C71-84D7-E38A69F123DD")!, contentId: "1", mediaType: .sound, eventType: .created),
-            .init(id: UUID(uuidString: "EB73727F-E228-4025-9109-886A9C33CAC5")!, contentId: "1", mediaType: .sound, eventType: .created),
-            .init(id: UUID(uuidString: "DC15BD2A-88BE-4C4D-9AE5-790856FAC919")!, contentId: "1", mediaType: .sound, eventType: .created),
-            .init(id: UUID(uuidString: "22356F60-4F21-4847-821A-B4D228E3B161")!, contentId: "1", mediaType: .sound, eventType: .created),
-            .init(id: UUID(uuidString: "58604F80-8840-4858-87DD-65EF4D016C37")!, contentId: "1", mediaType: .sound, eventType: .created)
-        ]
+        syncService.updates = mockUpdates
 
         localDatabase.preexistingUpdates = [
             .init(id: UUID(uuidString: "6AE46488-4872-4C71-84D7-E38A69F123DD")!, contentId: "1", mediaType: .sound, eventType: .created),
@@ -57,5 +93,7 @@ extension SyncManagerTests {
         await sut.sync()
 
         XCTAssertEqual(localDatabase.numberOfTimesInsertUpdateEventWasCalled, 2)
+        XCTAssertEqual(logger.errorHistory.count, 0)
+        XCTAssertEqual(logger.successHistory.count, 0) // Maybe this should be 1?
     }
 }
