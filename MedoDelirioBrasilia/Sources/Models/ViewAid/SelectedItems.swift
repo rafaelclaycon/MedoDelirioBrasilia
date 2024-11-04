3b6a662a1a09dc59d5da8bba7ed07beb079acb51
Rafael Schmitt <rafael.claycon@gmail.com>
2024-11-03 21:12:41 -0300

    Refactoring AddToFolderView

---
+++ MedoDelirioBrasilia/Sources/Models/ViewAid/SelectedItems.swift
@@ -0,0 +1,14 @@
+//
+//  SelectedItems.swift
+//  MedoDelirioBrasilia
+//
+//  Created by Rafael Schmitt on 03/11/24.
+//
+
+import Foundation
+
+struct SelectedItems: Identifiable {
+
+    let id = UUID()
+    let sounds: [Sound]
+}
--- MedoDelirioBrasilia/Sources/Repositories/UserFolderRepository.swift
+++ MedoDelirioBrasilia/Sources/Repositories/UserFolderRepository.swift
@@ -9,12 +9,22 @@ import Foundation
 
 protocol UserFolderRepositoryProtocol {
 
+    // Create
+
     func add(_ userFolder: UserFolder) throws
+    func add(sounds: [Sound], into folderId: String) throws
+
+    // Read
+
+    func allFolders() throws -> [UserFolder]
+
+    // Update
 
     func update(_ userFolder: UserFolder) throws
-
     /// Should only be used once.
     func addHashToExistingFolders() throws
+
+    // Delete
 }
 
 final class UserFolderRepository: UserFolderRepositoryProtocol {
@@ -28,16 +38,44 @@ final class UserFolderRepository: UserFolderRepositoryProtocol {
     ) {
         self.database = database
     }
+}
+
+// MARK: - Create
+
+extension UserFolderRepository {
 
     func add(_ userFolder: UserFolder) throws {
         var newFolder = userFolder
+        newFolder.name = newFolder.name.trimmingCharacters(in: .whitespacesAndNewlines)
         newFolder.changeHash = FolderResearchProvider.hash(userFolder.folderHash([]))
         try database.insert(newFolder)
     }
 
+    func add(sounds: [Sound], into folderId: String) throws {
+        try sounds.forEach { sound in
+            try database.insert(contentId: sound.id, intoUserFolder: folderId)
+        }
+        // Update folder hash
+    }
+}
+
+// MARK: - Read
+
+extension UserFolderRepository {
+
+    func allFolders() throws -> [UserFolder] {
+        try database.allFolders()
+    }
+}
+
+// MARK: - Update
+
+extension UserFolderRepository {
+
     func update(_ userFolder: UserFolder) throws {
         var folder = userFolder
         let contents = try database.contentsInside(userFolder: folder.id)
+        folder.name = folder.name.trimmingCharacters(in: .whitespacesAndNewlines)
         folder.changeHash = folder.folderHash(contents.map { $0.contentId })
         try database.update(folder)
     }
