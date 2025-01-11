b1973fb156b8b4dc1a134e40dc33c4161a29a053
Rafael Schmitt <rafael.claycon@gmail.com>
2025-01-11 20:56:01 -0300

    Add Spotlight index adding

---
+++ MedoDelirioBrasilia/Sources/Views/Sounds/MainSoundContainerViewModel+SpotlightIndex.swift
@@ -0,0 +1,35 @@
+//
+//  MainSoundContainerViewModel+SpotlightIndex.swift
+//  MedoDelirioBrasilia
+//
+//  Created by Rafael Schmitt on 11/01/25.
+//
+
+import Foundation
+import CoreSpotlight
+
+extension MainSoundContainerViewModel {
+
+    public func index(sounds: [Sound]) {
+        sounds.forEach { sound in
+            addContentToIndex(sound)
+        }
+    }
+
+    private func addContentToIndex(_ sound: Sound) {
+        let attributeSet = CSSearchableItemAttributeSet(contentType: UTType.audio)
+        attributeSet.title = sound.title
+        attributeSet.artist = sound.authorName
+        attributeSet.duration = NSNumber(value: sound.duration)
+
+        let item = CSSearchableItem(uniqueIdentifier: sound.id, domainIdentifier: nil, attributeSet: attributeSet)
+
+        let defaultIndex = CSSearchableIndex.default()
+        defaultIndex.indexSearchableItems([item]) { error in
+            if let error {
+                return print(error.localizedDescription)
+            }
+            print("\(sound.title) indexed.")
+        }
+    }
+}
