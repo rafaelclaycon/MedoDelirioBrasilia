bc6cb8646e7c3d110748868f5272f5bb5446c8f3
Rafael Schmitt <rafael.claycon@gmail.com>
2025-03-24 15:49:08 -0300

    Basics in place

--- MedoDelirioBrasilia/Sources/Views/Trends/TopChartRow.swift
+++ MedoDelirioBrasilia/Sources/Views/Trends/TopChartRow.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+enum TopChartPlace {
+
+    case first, second, third, other
+}
+
 struct TopChartRow: View {
 
     let item: TopChartItem
@@ -31,47 +36,111 @@ struct TopChartRow: View {
         UIDevice.isMac
     }
 
+    private var isSpecialCase: Bool {
+        ["1","2","3"].contains(item.rankNumber)
+    }
+
+    private var place: TopChartPlace {
+        switch item.rankNumber {
+        case "1": return .first
+        case "2": return .second
+        case "3": return .third
+        default: return .other
+        }
+    }
+
+    // MARK: - View Body
+
     var body: some View {
-        HStack(spacing: 15) {
-            NumberBadgeView(number: item.rankNumber, showBackgroundCircle: !showStripedList)
+        VStack {
+            if isSpecialCase {
+                SpecialRow(item: item, place: place)
+            } else {
+                HStack(spacing: 15) {
+                    NumberBadgeView(number: item.rankNumber, showBackgroundCircle: !showStripedList)
 
-            VStack(alignment: .leading, spacing: 5) {
-                Text(item.contentName)
-                    .bold()
-                Text(item.contentAuthorName)
-                    .foregroundStyle(.gray)
-                    .lineLimit(1)
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(item.contentName)
+                            .bold()
+                        Text(item.contentAuthorName)
+                            .foregroundStyle(.gray)
+                            .lineLimit(1)
+                    }
+
+                    Spacer()
+
+                    Text("\(item.shareCount)")
+                }
+                .background(.background)
+                .overlay {
+                    if showStripedList {
+                        RoundedRectangle(cornerRadius: 5, style: .continuous)
+                            .fill(cellFill)
+                            .opacity(0.2)
+                    }
+                }
             }
-
-            Spacer()
-
-            Text("\(item.shareCount)")
         }
         .padding(.horizontal)
         .padding(.vertical, 14)
-        .background(.background)
-        .overlay {
-            if showStripedList {
-                RoundedRectangle(cornerRadius: 5, style: .continuous)
-                    .fill(cellFill)
-                    .opacity(0.2)
+    }
+}
+
+// MARK: - Subviews
+
+extension TopChartRow {
+
+    struct SpecialRow: View {
+
+        let item: TopChartItem
+        let place: TopChartPlace
+
+        var body: some View {
+            HStack(spacing: 15) {
+                switch place {
+                case .first:
+                    Text("🥇")
+                        .font(.system(size: 54))
+                case .second:
+                    Text("🥈")
+                        .font(.system(size: 54))
+                case .third:
+                    Text("🥉")
+                        .font(.system(size: 54))
+                case .other:
+                    EmptyView()
+                }
+
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(item.contentName)
+                        .bold()
+                    Text(item.contentAuthorName)
+                        .foregroundStyle(.gray)
+                        .lineLimit(1)
+                }
+
+                Spacer()
+
+                Text("\(item.shareCount)")
+                    .bold(place == .first)
             }
+            .background(.background)
         }
     }
 }
 
-struct TopChartCellView_Previews: PreviewProvider {
+// MARK: - Preview
 
-    static var previews: some View {
-        Group {
-            TopChartRow(item: TopChartItem(id: "ABCD-EFGH",
-                                                rankNumber: "1",
-                                                contentId: "ABC",
-                                                contentName: "Olha que imbecil",
-                                                contentAuthorId: "DEF",
-                                                contentAuthorName: "Bolsonaro",
-                                                shareCount: 15))
-        }
-        .previewLayout(.fixed(width: 300, height: 100))
+#Preview {
+    Group {
+        TopChartRow(item: TopChartItem(
+            id: "ABCD-EFGH",
+            rankNumber: "1",
+            contentId: "ABC",
+            contentName: "Olha que imbecil",
+            contentAuthorId: "DEF",
+            contentAuthorName: "Bolsonaro",
+            shareCount: 15
+        ))
     }
 }
