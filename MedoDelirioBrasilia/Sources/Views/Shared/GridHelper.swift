fee98df22e8ed8a84ee20e9537c9763f8c81aabf
rafaelschmittwt <rafael.schmitt@willowtreeapps.com>
2025-04-12 14:41:32 -0300

    Remove unused GridHelper func

--- MedoDelirioBrasilia/Sources/Views/Shared/GridHelper.swift
+++ MedoDelirioBrasilia/Sources/Views/Shared/GridHelper.swift
@@ -2,46 +2,6 @@ import SwiftUI
 
 class GridHelper {
 
-    @available(*, deprecated, message: "Kept just to avoid breaking old code. Use adaptableColumns() instead.")
-    static func soundColumns(listWidth: CGFloat, sizeCategory: ContentSizeCategory) -> [GridItem] {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            if sizeCategory > ContentSizeCategory.large {
-                return [
-                    GridItem(.flexible())
-                ]
-            } else {
-                return [
-                    GridItem(.flexible()),
-                    GridItem(.flexible())
-                ]
-            }
-        } else {
-            if listWidth < 500 {
-                return [
-                    GridItem(.flexible())
-                ]
-            } else if listWidth < 600 {
-                return [
-                    GridItem(.flexible()),
-                    GridItem(.flexible())
-                ]
-            } else if listWidth < 705 {
-                return [
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible())
-                ]
-            } else {
-                return [
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible())
-                ]
-            }
-        }
-    }
-
     static func adaptableColumns(
         listWidth: CGFloat,
         sizeCategory: ContentSizeCategory,
