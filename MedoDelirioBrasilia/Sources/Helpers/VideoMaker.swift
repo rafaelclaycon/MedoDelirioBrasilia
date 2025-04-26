94058fd00e052ce3110d3baf650027148cffd6c3
rafaelschmittwt <rafael.schmitt@willowtreeapps.com>
2025-04-26 15:30:08 -0300

    Remove func marked for removal

--- MedoDelirioBrasilia/Sources/Helpers/VideoMaker.swift
+++ MedoDelirioBrasilia/Sources/Helpers/VideoMaker.swift
@@ -45,29 +45,6 @@ class VideoMaker {
         }
     }
     
-    // TODO: - Remove this method when dropping support for iOS 15.
-    static func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
-        let textColor = UIColor.black
-        let textFont = UIFont.systemFont(ofSize: 72, weight: .bold)
-
-        let scale = UIScreen.main.scale
-        UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
-
-        let textFontAttributes = [
-            NSAttributedString.Key.font: textFont,
-            NSAttributedString.Key.foregroundColor: textColor,
-            ] as [NSAttributedString.Key : Any]
-        image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
-
-        let rect = CGRect(origin: point, size: CGSize(width: image.size.width - 80, height: image.size.height))
-        text.draw(in: rect, withAttributes: textFontAttributes)
-
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        return newImage!
-    }
-    
     static func mergeVideoWithAudio(
         videoUrl: URL,
         audioUrl: URL,
