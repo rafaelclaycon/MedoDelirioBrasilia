79acc776893f0004960ce3d0f5316bee7139bbb5
rafaelschmittwt <rafael.schmitt@willowtreeapps.com>
2025-05-04 13:22:50 -0300

    Fix HelpView

--- MedoDelirioBrasilia/Sources/Views/Settings/Sub-Settings/Help/HelpView.swift
+++ MedoDelirioBrasilia/Sources/Views/Settings/Sub-Settings/Help/HelpView.swift
@@ -8,125 +8,124 @@
 import SwiftUI
 
 struct HelpView: View {
-    
-    private let iconFrameWidth: CGFloat = 40
-    
-    private var toPlayInstruction: String {
-        if ProcessInfo.processInfo.isMacCatalystApp {
-            return "Para reproduzir um som, clique nele 1 vez. Para parar de reproduzir, clique nele novamente."
-        } else {
-            return "Para reproduzir um som, toque nele 1 vez. Para parar de reproduzir, toque nele novamente."
-        }
-    }
-    
-    private var toShareInstruction: String {
-        if ProcessInfo.processInfo.isMacCatalystApp {
-            return "Para compartilhar, clique com o botão direito no som e escolha Compartilhar Som."
-        } else {
-            return "Para compartilhar, segure o som e escolha Compartilhar Som."
-        }
-    }
-    
-    private var toSearchInstruction: String {
-        if ProcessInfo.processInfo.isMacCatalystApp {
-            return "Para pesquisar, clique no campo Buscar no canto superior direito da tela de sons e digite o texto que procura.\n\nA pesquisa considera o conteúdo do áudio e o nome do autor. Não use vírgulas."
-        } else {
-            if UIDevice.current.userInterfaceIdiom == .phone {
-                return "Para pesquisar, vá até o topo da lista de sons e puxe mais um pouco para baixo até revelar o campo Buscar.\n\nA pesquisa considera o conteúdo do áudio e o nome do autor. Não use vírgulas."
-            } else {
-                return "Para pesquisar, toque no campo Buscar no canto superior direito da tela de sons e digite o texto que procura.\n\nA pesquisa considera o conteúdo do áudio e o nome do autor. Não use vírgulas."
-            }
-        }
-    }
-    
-    private var favoritesInstruction: String {
-        if ProcessInfo.processInfo.isMacCatalystApp {
-            return "Para favoritar, clique com o botão direito em um som e escolha Adicionar aos Favoritos.\n\nPara ver apenas os favoritos, clique em Favoritos na barra lateral.\n\nÉ possível pesquisar entre os favoritos usando o campo de Busca no topo direito da tela de Favoritos."
-        } else {
-            if UIDevice.current.userInterfaceIdiom == .phone {
-                return "Para favoritar, segure o som e escolha Adicionar aos Favoritos.\n\nPara ver apenas os favoritos, toque em Favoritos na barra flutuante na parte inferior da tela.\n\nÉ possível pesquisar entre os favoritos usando a barra de Busca. Para isso, na lista de favoritos, vá até o topo e puxe mais um pouco para baixo até ver a barra."
-            } else {
-                return "Para favoritar, segure o som e escolha Adicionar aos Favoritos.\n\nPara ver apenas os favoritos, toque em Favoritos na barra lateral.\n\nÉ possível pesquisar entre os favoritos usando o campo de Busca no topo direito da tela de Favoritos."
-            }
-        }
-    }
 
     var body: some View {
         VStack {
             ScrollView {                
-                VStack(alignment: .leading, spacing: 30) {
-                    HStack {
-                        Text("Sons")
-                            .font(.title)
-                            .bold()
-                        
-                        Spacer()
-                    }
-                    
-                    HStack(alignment: .center, spacing: 15) {
-                        Image(systemName: "play.fill")
-                            .font(.largeTitle)
-                            .foregroundColor(.accentColor)
-                            .frame(width: iconFrameWidth)
-                        
-                        Text(toPlayInstruction)
-                    }
-                    
-                    HStack(alignment: .center, spacing: 15) {
-                        Image(systemName: "square.and.arrow.up")
-                            .font(.largeTitle)
-                            .foregroundColor(.accentColor)
-                            .frame(width: iconFrameWidth)
-                        
-                        Text(toShareInstruction)
-                    }
+                VStack(alignment: .leading, spacing: .spacing(.xxLarge)) {
+                    Text("Sons & Músicas")
+                        .font(.title)
+                        .bold()
+
+                    HelpInstructionView(
+                        symbol: "play.fill",
+                        text: toPlayInstruction
+                    )
+
+                    HelpInstructionView(
+                        symbol: "square.and.arrow.up",
+                        text: toShareInstruction
+                    )
                     
                     Divider()
-                    
-                    HStack(alignment: .center, spacing: 15) {
-                        Image(systemName: "magnifyingglass")
-                            .font(.largeTitle)
-                            .foregroundColor(.accentColor)
-                            .frame(width: iconFrameWidth)
-                        
-                        Text(toSearchInstruction)
-                    }
+
+                    HelpInstructionView(
+                        symbol: "magnifyingglass",
+                        text: toSearchInstruction
+                    )
                     
                     Divider()
-                    
-                    HStack(alignment: .center, spacing: 15) {
-                        Image(systemName: "star.fill")
-                            .font(.largeTitle)
-                            .foregroundColor(.red)
-                            .frame(width: iconFrameWidth)
 
-                        Text(favoritesInstruction)
-                    }
-                    
-                    HStack {
-                        Text("Músicas")
-                            .font(.title)
-                            .bold()
-
-                        Spacer()
-                    }
-                    
-                    SongHelpView()
-                        .padding(.bottom, 15)
+                    HelpInstructionView(
+                        symbol: "star.fill",
+                        color: .red,
+                        text: favoritesInstruction
+                    )
                 }
-                .padding()
+                .padding(.horizontal, .spacing(.medium))
+                .padding(.vertical, .spacing(.xSmall))
             }
         }
         .navigationTitle("Ajuda")
         .navigationBarTitleDisplayMode(.inline)
     }
-
 }
 
-struct SoundHelpView_Previews: PreviewProvider {
+// MARK: - Text
 
-    static var previews: some View {
-        HelpView()
+extension HelpView {
+
+    private var toPlayInstruction: String {
+        if UIDevice.isMac {
+            return "Para reproduzir um conteúdo, clique nele 1 vez. Para parar de reproduzir, clique nele novamente."
+        } else {
+            return "Para reproduzir um conteúdo, toque nele 1 vez. Para parar de reproduzir, toque nele novamente."
+        }
     }
 
+    private var toShareInstruction: String {
+        if UIDevice.isMac {
+            return "Para compartilhar, clique com o botão direito no conteúdo e escolha Compartilhar."
+        } else {
+            return "Para compartilhar, segure o conteúdo por alguns segundos até que o menu de contexto abra e escolha Compartilhar."
+        }
+    }
+
+    private var toSearchInstruction: String {
+        if UIDevice.isMac {
+            return "Para pesquisar, clique no campo Buscar no canto superior direito da tela de sons e digite o texto que procura.\n\nA pesquisa considera o que é falado no áudio e o nome do autor ou gênero musical. Não use vírgulas."
+        } else {
+            if UIDevice.isiPhone {
+                return "Para pesquisar, vá até o topo da lista de conteúdos e puxe mais um pouco para baixo até revelar o campo Buscar.\n\nA pesquisa considera o que é falado no áudio e o nome do autor ou gênero musical. Não use vírgulas."
+            } else {
+                return "Para pesquisar, toque no campo Buscar no canto superior direito da tela de sons e digite o texto que procura.\n\nA pesquisa considera o que é falado no áudio e o nome do autor ou gênero musical. Não use vírgulas."
+            }
+        }
+    }
+
+    private var favoritesInstruction: String {
+        if UIDevice.isMac {
+            return "Para favoritar, clique com o botão direito em um conteúdo e escolha Adicionar aos Favoritos.\n\nPara ver apenas os favoritos, clique em Favoritos na barra lateral.\n\nÉ possível pesquisar entre os favoritos usando o campo Buscar no topo direito da tela de Favoritos."
+        } else {
+            if UIDevice.isiPhone {
+                return "Para favoritar, segure o conteúdo e escolha Adicionar aos Favoritos.\n\nPara ver apenas os favoritos, toque em Favoritos nos filtros da parte superior da tela.\n\nÉ possível pesquisar entre os favoritos usando a barra de Busca. Para isso, na lista de favoritos, vá até o topo e puxe mais um pouco para baixo até ver a barra."
+            } else {
+                return "Para favoritar, segure o conteúdo e escolha Adicionar aos Favoritos.\n\nPara ver apenas os favoritos, toque em Favoritos na barra lateral.\n\nÉ possível pesquisar entre os favoritos usando o campo Buscar no topo direito da tela de Favoritos."
+            }
+        }
+    }
+}
+
+// MARK: - Subviews
+
+extension HelpView {
+
+    struct HelpInstructionView: View {
+
+        let symbol: String
+        var color: Color = .accentColor
+        let iconFrameWidth: CGFloat = 40
+        let text: String
+
+        var body: some View {
+            HStack {
+                Image(systemName: symbol)
+                    .font(.largeTitle)
+                    .foregroundColor(color)
+                    .frame(width: iconFrameWidth)
+                    .padding(.leading, .spacing(.xxxSmall))
+                    .padding(.trailing, .spacing(.xSmall))
+
+                Text(text)
+            }
+        }
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    NavigationStack {
+        HelpView()
+    }
 }
