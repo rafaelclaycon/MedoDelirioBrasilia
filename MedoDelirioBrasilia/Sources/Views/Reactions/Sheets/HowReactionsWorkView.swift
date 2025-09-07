a5cc63ad5ecfe90c4c38fee67f103debdcee0e05
Rafael Schmitt <132580041+rafaelschmittwt@users.noreply.github.com>
2025-09-07 19:21:22 -0300

    Update About Reactions text

--- MedoDelirioBrasilia/Sources/Views/Reactions/Sheets/HowReactionsWorkView.swift
+++ MedoDelirioBrasilia/Sources/Views/Reactions/Sheets/HowReactionsWorkView.swift
@@ -16,7 +16,7 @@ struct HowReactionsWorkView: View {
             ScrollView {
                 VStack(alignment: .center, spacing: 30) {
                     VStack(spacing: 0) {
-                        Text("Apresentando as ")
+                        Text("Sobre as ")
                             .font(.largeTitle)
                             .bold()
                             .foregroundColor(.primary)
@@ -40,7 +40,7 @@ struct HowReactionsWorkView: View {
                     }
                     .multilineTextAlignment(.center)
 
-                    Text("Descubra os sons de um jeito novo.\n\nEm 2 anos, fomos de 0 a mais de 1.200 sons. Essa quantidade trouxe, porém, problemas de descoberta. Os sons mais compartilhados são os adicionados a menos tempo ou os que mais grudaram na cabeça da galera, mas tem muita vírgula boa escondida! Pensando nisso, chegaram as Reações.\n\nEscolha a categoria que melhor define como você quer responder ou começar uma conversa. Em seguida, use um dos sons para responder a uma mensagem ou post rapidamente.\n\nAquele “Tadinha! Que barra!” ou “Mas isso é… É enganar!” que colocados na hora certa fazem toda a diferença.")
+                    Text("A aba Reações é um jeito diferente de descubrir as vírgulas sonoras.\n\nEm 3 anos, chegamos a mais de 1.400 vírgulas, mas muitas acabam escondidas. Para facilitar a descoberta, criamos as Reações: escolha uma categoria e responda rápido com o som perfeito.\n\nUm “Tadinha! Que barra!” ou “Mas isso é… É enganar!” na hora certa muda tudo.")
                         .multilineTextAlignment(.center)
                 }
                 .padding(.horizontal, 30)
@@ -48,7 +48,7 @@ struct HowReactionsWorkView: View {
             }
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
-                    Button("Fechar") {
+                    CloseButton {
                         dismiss()
                     }
                 }
