2d7e184ed4f8d6c398a4e7741edaedf3d52d5256
Rafael Schmitt <rafael.claycon@gmail.com>
2025-09-13 19:46:39 -0300

    Fix Share unavailable on macOS 26

--- MedoDelirioBrasilia/Sources/Views/ShareAsVideo/ShareAsVideoView.swift
+++ MedoDelirioBrasilia/Sources/Views/ShareAsVideo/ShareAsVideoView.swift
@@ -41,7 +41,7 @@ struct ShareAsVideoView: View {
     }
 
     private var isShareAvailable: Bool {
-        if #available(macOS 26, *) {
+        if #available(macOS 26, *), UIDevice.isMac {
             return false
         } else {
             return true
